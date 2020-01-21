a6f34557e72a8d9e003edf00049775ae260fdaab
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-01-21 17:42:54 +0000

    AULA_APOIO

---
+++ server/src/email.ts
@@ -0,0 +1,15 @@
+ 
+/**
+ * @param from: string,
+ * @param to: string,
+ * @param subject: string,
+ * @param message: string
+**/
+
+function sendEmail(
+    from: string,
+    to: string,
+    subject: string,
+    message: string){
+
+    }
