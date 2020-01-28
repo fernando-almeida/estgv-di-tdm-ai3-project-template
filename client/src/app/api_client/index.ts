11a9aa9e99a58139fe5f16f51019e16b413a4aa6
francisco17352 <57689459+francisco17352@users.noreply.github.com>
2020-01-28 12:15:43 +0000

    update

---
+++ client/src/app/api_client/index.ts
@@ -0,0 +1,5 @@
+export * from './api/api';
+export * from './model/models';
+export * from './variable';
+export * from './configuration';
+export * from './api.module';
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,29 +13,30 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { environment } from '../environments/environment';
+import { ApiModule } from './api_client/api.module';
+import { Configuration, ConfigurationParameters } from './api_client/configuration';
+import { BASE_PATH } from './api_client/variable';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 import { MaterialLayoutComponent } from './material-layout/material-layout.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HomeComponent } from './Home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LogintesteComponent } from './loginteste/loginteste.component';
-import { RegistarComponent } from './registar/registar.component';
-import { MudarPassComponent } from './mudar-pass/mudar-pass.component';
-import { EmailComponent } from './email/email.component';
+
+/**
+ * Build API configuration
+ */
+function buildApiConfiguration() {
+  const configurationParameters: ConfigurationParameters = {};
+  // TODO: Token should be injected using HTTP Interceptor pattern (@see link in Moodle)
+  const config = new Configuration(configurationParameters);
+  return config;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     MaterialLayoutComponent,
-    NavBarComponent,
-    ProfileComponent,
-    HomeComponent,
-    LogintesteComponent,
-    RegistarComponent,
-    MudarPassComponent,
-    EmailComponent
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
@@ -52,9 +53,13 @@ import { EmailComponent } from './email/email.component';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    ApiModule.forRoot(buildApiConfiguration),
   ],
-  providers: [],
+  providers: [
+    // Hard-coded on API *Service classes but can be overriden here
+    { provide: BASE_PATH, useValue: environment.apiBaseUrl },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
